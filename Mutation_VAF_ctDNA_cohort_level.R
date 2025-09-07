63d2ee7236ea253119e350812fb2edad3bec1533
Virdzek-g <82646598+virdzek-g@users.noreply.github.com>
2025-09-07 13:09:57 -0400

    Update Mutation_VAF_ctDNA_cohort_level.R

--- Mutation_VAF_ctDNA_cohort_level.R
+++ Mutation_VAF_ctDNA_cohort_level.R
@@ -1,7 +1,7 @@
 # Specify sample order
-set_order_of_time_points <- function(time_col) {
+set_order_of_samples <- function(sample_col) {
   sample_order <- c('Sample1')
-  return(factor(time_col, levels = sample_order))
+  return(factor(sample_col, levels = sample_order))
 }
 
 
@@ -10,21 +10,21 @@ set_order_of_time_points <- function(time_col) {
 add_color_columns <- function(data) {
   data$fill_color <- ifelse(grepl("multi", data$Genes), "#D4AC0D",
                             ifelse(grepl("cfDNA", data$Genes), "transparent",
-                                   ifelse(data$Time.point == "T", "#E77B7B", "#2980B9")))
+                                   ifelse(data$Sample == "T", "#E77B7B", "#2980B9")))
   data$border_color <- ifelse(grepl("multi", data$Genes), "transparent",
                               ifelse(grepl("cfDNA", data$Genes), "black",
-                                     ifelse(data$Time.point == "T", "transparent", "transparent")))
+                                     ifelse(data$Sample == "T", "transparent", "transparent")))
   return(data)
 }
 
-plot_gene_vaf_long <- function(table, gene_col, time_col, vaf_col, dot_size, label_size, line_color = "black") {
+plot_gene_vaf_long <- function(table, gene_col, sample_col, vaf_col, dot_size, label_size, line_color = "black") {
   # Convert 'Genes' to factor 
   table[[gene_col]] <- factor(table[[gene_col]], levels = unique(table[[gene_col]]))
   # Reorder the data based on the order of samples
-  table[[time_col]] <- set_order_of_time_points(table[[time_col]])
+  table[[sample_col]] <- set_order_of_time_points(table[[sample_col]])
 
-  p <- ggplot(table, aes(x = table[[time_col]], y = factor(table[[gene_col]], levels = unique(table[[gene_col]])))) +
-    geom_segment(aes(xend = table[[time_col]], yend = factor(table[[gene_col]], levels = unique(table[[gene_col]])), color = line_color), size = 1) +
+  p <- ggplot(table, aes(x = table[[sample_col]], y = factor(table[[gene_col]], levels = unique(table[[gene_col]])))) +
+    geom_segment(aes(xend = table[[sample_col]], yend = factor(table[[gene_col]], levels = unique(table[[gene_col]])), color = line_color), size = 1) +
     geom_point(aes(size = table[[vaf_col]], color = border_color, fill = fill_color), 
                shape = 21, stroke = 0.5) +
     scale_size_continuous(range = c(1, 10) * dot_size, 
@@ -45,7 +45,7 @@ plot_gene_vaf_long <- function(table, gene_col, time_col, vaf_col, dot_size, lab
   print(p)
 }
 
-### data table should contain 3 columns Genes, Sample or Time.point and VAF
+### data table should contain 3 columns Genes, Sample or Sample and VAF
 # Sample and Time point are used interchangeably, depending on what needs to be on x axis
 
 data <- read.delim()
@@ -55,11 +55,11 @@ data1$Genes <- sub('.*-','',data1$Genes)
 
 # parameters
 gene_col <- "Genes"
-time_col <- "Time.point"
+sample_col <- "Sample"
 vaf_col <- "VAF"
 dot_size <- 0.7                    
 label_size <- 10                       
 line_color <- "black"                   
 
 
-plot_gene_vaf_long(data1, gene_col, time_col, vaf_col, dot_size, label_size, line_color)
+plot_gene_vaf_long(data1, gene_col, sample_col, vaf_col, dot_size, label_size, line_color)
